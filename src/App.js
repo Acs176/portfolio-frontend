41f97f928b5f1b69bffe8175190c4f4d8e142aee
Acs176 <acs176@alu.ua.es>
2024-07-22 23:06:39 +0200

    coded basic structure and basic styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,40 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      hello
+      <div className='left-panel'>
+        <div className='left-panel-inside'>
+          <div className='div-title'>
+            <h1 id='my-name'>ADRIÁN CARBONELL</h1>
+            <h2>SOFTWARE ENGINEER</h2>
+          </div>
+          <ul>
+            <li>STUDIES</li>
+            <li>EXPERIENCE</li>
+            <li>PROJECTS</li>
+          </ul>
+        </div>
+      </div>
+      <div className='left-panel-placeholder'>
+
+      </div>
+      <div className='right-panel'>
+        <div className='right-panel-inside'>
+          <div className='quote'>
+            <p>‘‘Luck is what happens when preparation meets opportunity.’’</p>
+            <p>- Seneca</p>
+          </div>
+          <div className='paragraph'>
+            <p>
+              Welcome to my portfolio! I am a passionate software engineer dedicated to exploring the vast potential of deep learning and mastering the intricacies of backend development. My journey in technology is driven by a relentless pursuit of excellence and a commitment to continuous professional growth.
+            </p>
+            <p>
+              Here, you’ll find a showcase of my work, highlighting my technical expertise, personal projects, and the strides I’ve made in the world of software engineering. Thank you for visiting, and I look forward to connecting with fellow enthusiasts and collaborators in this exciting field.
+            </p>
+          </div>
+          
+        </div>
+        
+      </div>
     </div>
   );
 }
